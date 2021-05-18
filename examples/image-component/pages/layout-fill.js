5892ef0629e2d5001ab36761280562ecaa943469
Fernando Rojo <frojo@sas.upenn.edu>
2021-05-18 14:08:41 -0400

    Update layout-fill.js

--- examples/image-component/pages/layout-fill.js
+++ examples/image-component/pages/layout-fill.js
@@ -6,7 +6,16 @@ const itemWidth = '20vh'
 const Fill = () => (
   <div>
     <ViewSource pathname="pages/layout-fill.js" />
-    <h1>Breaks on Safari 12 and lower</h1> 
+    <h1>Breaks on Safari 12 and lower</h1>  
+    <div style={{ position: 'relative', width: '300px', height: '500px' }}>
+      <Image
+        alt="Mountains"
+        src="/mountains.jpg"
+        layout="fill"
+        objectFit="cover"
+        sizes={`(max-width: 100vw) ${itemWidth}, ${itemWidth}`}
+      />
+    </div> 
     
     <div style={{ position: 'relative', width: '300px', height: '500px' }}>
       <Image
